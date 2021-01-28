02d6c3df73ac4e392f87ec1f0dce4baf17e2e7eb
Hyumari <78027104+Hyumari@users.noreply.github.com>
2021-01-28 15:27:41 +0100

    first commit milan
    oefenbestanden toegevoegd

---
+++ BOZWEAPONS.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>WEAPONS</title>
+    <link rel="stylesheet" href="css/stylesheet.css">
+</head>
+<body>
+	<ul>
+    <li><a href="/">MENU</a></li>
+    <li><a href="BOZMAPS.php">MAPS</a></li>
+    <li><a href="BOZWEAPONS.php">WEAPONS</a></li>
+    <li><a href="BOZCHARACTERS.php">CHARACTERS</a></li>
+	</ul>
+
+<h1>WEAPONS</h1>
+	
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>MENU</title>
+     <link rel="stylesheet" href="css/stylesheet.css">
+</head>
+<body>
+	<ul>
+		<li><a href="/">MENU</a></li>
+		<li><a href="BOZMAPS.php">MAPS</a></li>
+		<li><a href="BOZWEAPONS.php">WEAPONS</a></li>
+		<li><a href="BOZCHARACTERS.php">CHARACTERS</a></li>
+	</ul>
+
+<h1>MENU</h1>
+	
+</body>
+</html>
