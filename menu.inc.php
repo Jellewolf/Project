dd01b79daeea35d549883db3ccc1e4a7eb03d3b0
Hyumari <78027104+Hyumari@users.noreply.github.com>
2021-02-04 13:21:22 +0100

    1.2

--- menu.inc.php
+++ menu.inc.php
@@ -1,17 +1,19 @@
-
 <!DOCTYPE html>
 <html>
 <head>
-  <title>menuandlogo</title>
+  <title>menu</title>
      <link rel="stylesheet" href="css/stylesheet.css">
 </head>
 <body>
-	<ul>
-		<li><a href="home.php">Home</a></li>
-		<li><a href="service.php">Service</a></li>
-		<li><a href="voorlichting.php">Voorlichting</a></li>
-		<li><a href="login.php">Login</a></li>
-    </ul>
-    <!--This is a menu include not a stand alone file just a file for the main menu/logo-->
+	<img src="images/logo-placeholder.png" alt="placeholder" width="500" height="200">
+		<h1>Menu</h1>
+			<ul>
+				<li><a href="home.php">Home</a></li>
+				<li><a href="service.php">Service</a></li>
+				<li><a href="voorlichting.php">voorlichting</a></li>
+				<li><a href="login.php">Login</a></li>
+   	 		</ul>
+   	 					<p>test</p>
+    		<!--This is a menu include not a stand alone file just a file for the main menu/logo-->
 </body>
 </html>
