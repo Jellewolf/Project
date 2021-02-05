be9f0ee96577c15e0ea3b32e7bfd5b6d30736678
Zabarion <63313141+Zabarion@users.noreply.github.com>
2021-02-05 19:44:31 +0100

    Added v1 of news article display and new database

--- home.php
+++ home.php
@@ -1,3 +1,6 @@
+<?php
+include_once 'inc/dbh.inc.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,6 +17,20 @@ include 'inc/menu.inc.php';
 <p class="herhaalservice">Herhaal Service</p>
 <p class="herhaalservice">Medicijnenoverzicht</p>
 <p class="herhaalservice">Inschrijven</p>
+<!-- DO NOT DELETE (This is the news data shown on the site and is a WIP)-->
+<p>
+<?php
+ $sql = "SELECT * FROM news order by NewsDate desc limit 3 offset 0;";
+ $result = mysqli_query($conn, $sql);
+ $resultcheck = mysqli_num_rows($result);
+
+if ($resultcheck > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo $row['NewsArticle'] . "<br />";
+    }
+}
+?>
+</p>
 </body>
 </html>
 
