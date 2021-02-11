d11a2875fe47e0169009e6e3f99990697228cea2
Zabarion <63313141+Zabarion@users.noreply.github.com>
2021-02-11 14:39:53 +0100

    Co-authored-by: Roodenburg, Arjan (257257) <257257@cursist.rockopnh.nl>

---
+++ signup.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Service</title>
+<?php
+include 'inc/stylesheet.inc.php';
+?>
+</head>
+<body>
+<?php
+include 'inc/menu.inc.php';
+?>
+<!-- This is the signup please do not delete-- <form action="inc/signup.inc.php" method="POST">
+-->
+<form action="inc/signup.inc.php" method="POST">
+ <input type="text" name="salutation" placeholder="Aanhef" required>
+ <br>
+ <input type="text" name="firstname" placeholder="Voornaam">
+ <br>
+ <input type="text" name="lastname" placeholder="Achternaam">
+ <br>
+ <input type="text" name="email" placeholder="E-mail">
+ <br>
+ <input type="text" name="userpass" placeholder="Wachtwoord">
+ <br>
+ <input type="text" name="phonenumber" placeholder="Telefoonnummer">
+ <br>
+ <input type="text" name="city" placeholder="Stad" required>
+ <br>
+ <input type="text" name="street" placeholder="Straat" required>
+ <br>
+ <input type="text" name="housenumber" placeholder="Huisnummer">
+ <br>
+ <input type="text" name="postalcode" placeholder="Postcode">
+ <br>
+ <button type="submit" name="submit">Registreren</button>
+</form>
+</body>
+</html>
