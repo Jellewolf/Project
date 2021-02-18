a26fa45281dba2c4cdc906073c2e093c5d63e6a8
Zabarion <63313141+Zabarion@users.noreply.github.com>
2021-02-18 18:48:46 +0100

    finished front end

--- home.php
+++ home.php
@@ -15,7 +15,7 @@ include 'inc/slideshow.inc.php'
 ?>
 <br>
 <h2>Onze Service</h2>
-<div class="test">
+<div class="main">
 	<p class="block1"><a href="/pages/repeatservice.php">Herhaal Service</a></p>
 	<p class="block1"><a href="/pages/medicationoverview.php">Medicijnenoverzicht</a></p>
 	<p class="block1"><a href="/pages/register.php">Inschrijven</a></p>
@@ -37,7 +37,12 @@ include 'inc/slideshow.inc.php'
 		<P>Op officiële feestdagen zijn beide locaties gesloten.</P>
 </div>
 	<div class="address">
-		<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+	<p>Contact
+        <p>ACN apotheek Schagen</p>
+        <p>Zuiderweg 23</p>
+        <p>1741 NA Schagen</p>
+        <p>Tel. 0224-297979</p>
+        <p>info@apotheekschagen.nl</p>
 	</div>
 </div>
 <div>
@@ -45,6 +50,15 @@ include 'inc/slideshow.inc.php'
 include 'inc/news.inc.php';
 ?>
 </div>
+<!--
+    <br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+<p class="footer">TEST</p> -->
 </body>
 </html>
 
