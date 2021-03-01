08e5dce23dff9116bee77778f5676acc62fcb3d5
Zabarion <63313141+Zabarion@users.noreply.github.com>
2021-03-01 13:01:13 +0100

    added styling to login farm and added news article

--- pages/login.php
+++ pages/login.php
@@ -7,23 +7,30 @@
 <link rel="icon" href="../images/logo.png">
 <link rel="stylesheet" href="/css/menu.inc.css">
 <link rel="stylesheet" href="/css/login.css">
+<link rel="stylesheet" href="/css/home.css">
 </head>
 <body>
+<div class="incmain">
 <?php
 include '../inc/menu.inc.php';
 ?>
+</div>
+<div class="logbox">
 <form>
- <input type="text" name="e-mail" placeholder="E-mail">
+ <input class="logform" type="text" name="e-mail" placeholder="E-mail">
  <br>
- <input type="text" name="username" placeholder="Gebruikersnaam">
+ <input class="logform" type="text" name="username" placeholder="Gebruikersnaam">
  <br>
- <input type="text" name="password" placeholder="Wachtwoord">
+ <input class="logform" type="text" name="password" placeholder="Wachtwoord">
  <br>
  <button type="submit" name="submit">Inloggen</button> 
  <br>
 <button type="link" name="signup"><a href="signup.php">signup</a></button>
  <br>
  <button type="link" name="reset"><a href="passreset.php">passreset</a></button>
-
+ </div>
+ <br>
+ <div>
+</div>
 </body>
 </html>
