36aa392ce3722d8b8335e2200e4ac9efa594c294
ArjanRoodenburg <78028416+ArjanRoodenburg@users.noreply.github.com>
2021-03-08 19:17:08 +0100

    Medicine overview basic structure and database connection

---
+++ inc/med.inc.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'dbh.inc.php';
+
+$sql = "SELECT * FROM medicine";
+$result = mysqli_query($conn, $sql);
+$resultcheck = mysqli_num_rows($result);
+
+if ($resultcheck > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<div class='medimg'><img src='/images/".$row['medicineimage']."'></div>";
+        echo "<div class='med'>".$row['medicinename']."</div>";
+        echo "<div class='medstock'>We hebben nog ".$row['medicinestock']." van dit medicijn over.</div>";
+      }
+}
+
+?>
