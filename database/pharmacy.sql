6a9af3b13bb557befd2cf4aa178d5e0342fade10
ArjanRoodenburg <78028416+ArjanRoodenburg@users.noreply.github.com>
2021-03-18 21:08:25 +0100

    Newest db with medicine

--- database/pharmacy.sql
+++ database/pharmacy.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 17 mrt 2021 om 22:43
+-- Gegenereerd op: 18 mrt 2021 om 21:04
 -- Serverversie: 10.4.17-MariaDB
--- PHP-versie: 8.0.1
+-- PHP-versie: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -56,7 +56,6 @@ INSERT INTO `contactform` (`firstname`, `lastname`, `phonenumber`, `email`, `con
 CREATE TABLE `medicine` (
   `medicinename` varchar(30) NOT NULL,
   `medicineimage` varchar(30) NOT NULL,
-  `medicinestock` int(11) NOT NULL,
   `medicineID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -64,12 +63,12 @@ CREATE TABLE `medicine` (
 -- Gegevens worden geëxporteerd voor tabel `medicine`
 --
 
-INSERT INTO `medicine` (`medicinename`, `medicineimage`, `medicinestock`, `medicineID`) VALUES
-('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy', 'placeholder1.png', 3, 9),
-('bb', 'placeholder1.png', 5, 10),
-('cc', 'placeholder1.png', 0, 11),
-('aa', 'placeholder1.png', 0, 12),
-('d', 'placeholder1.png', 0, 13);
+INSERT INTO `medicine` (`medicinename`, `medicineimage`, `medicineID`) VALUES
+('Abacavir', 'abacavir.jpg', 9),
+('Citalopram', 'citalopram.jpg', 10),
+('Oxazepam', 'oxazepam.jpeg', 11),
+('Haloperidol', 'haloperidol.jpg', 12),
+('Doxepine', 'doxepine.jpg', 13);
 
 -- --------------------------------------------------------
 
@@ -105,27 +104,30 @@ INSERT INTO `news` (`newsimages`, `newsname`, `newsarticle`, `newsdate`, `newsID
 
 CREATE TABLE `signup` (
   `salutation` varchar(100) DEFAULT NULL,
-  `useruid` varchar(100) NOT NULL,
-  `lastname` varchar(100) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `fastname` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `userpass` varchar(20) NOT NULL,
+  `phonenumber` varchar(50) NOT NULL,
   `city` varchar(100) DEFAULT NULL,
   `street` varchar(100) DEFAULT NULL,
   `housenumber` varchar(11) NOT NULL,
   `postalcode` varchar(100) NOT NULL,
-  `userID` int(11) NOT NULL
+  `loginID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `signup`
 --
 
-INSERT INTO `signup` (`salutation`, `useruid`, `lastname`, `email`, `userpass`, `city`, `street`, `housenumber`, `postalcode`, `userID`) VALUES
-('Dhr', 'Jelle', 'de Wolf', 'jellewolf3@gmail.com', '$2y$10$hG4GDXnnOABFE', '123', 'Den Hoorn', 'Zouteland', '9', 1),
-('Dhr', 'Jan', 'gekke man', 'jan@gmail.com', '$2y$10$Znp0bimxAanTC', '1234', 'appelstraat', 'appel1', '10', 2),
-('Dhr', 'gekkie', 'aap', 'aap@gmail.com', '$2y$10$XQQx8bzs.jJXM', 'lollol', 'dwadwadwa', 'dawdwad', 'wadwadwa', 3),
-('Dhr', 'Jelle', 'klaas', 'yolo@gmail.com', '$2y$10$rI2bcj1JdKgjX', '123456', 'okkk', 'kikiki', '9', 4),
-('Dhr', 'Jaap', 'dawd', 'jaap@gmail.com', '$2y$10$s0xTNyPOxXRLv', 'hoi', 'gdrgrdg', 'drgrdgrdg', '9', 5);
+INSERT INTO `signup` (`salutation`, `firstname`, `fastname`, `email`, `userpass`, `phonenumber`, `city`, `street`, `housenumber`, `postalcode`, `loginID`) VALUES
+('Dhr', 'Jelle', 'Wolf', 'jellewolf3@gmail.com', 'test', '06354354345', 'Den Hoorn', 'Zouteland', '9', '1797BA', 23),
+('Dhr', 'Jelle', 'Wolf', 'jellewolf3@gmail.com', 'test', '06354354345', 'Den Hoorn', 'Zouteland', '9', '1797BA', 24),
+('Dhr', 'Jelle', 'Wolf', 'jellewolf3@gmail.com', 'test4', '0615240373', 'Den Hoorn', 'Zouteland', 'Zouteland', '1797BA', 25),
+('aanhef hier', 'Arjan', 'Roodenburg', 'spuurmaann3@gmail.com', '110011', '110011', 'ANNA PAULOWNA', 'STATIONSWEG', 'STATIONSWEG', '1761EB', 26),
+('', 'Arjan', 'Roodenburg', 'spuurmaann3@gmail.com', '110011', '110011', 'ANNA PAULOWNA', 'STATIONSWEG', 'STATIONSWEG', '1761EB', 27),
+('', '', '', '', '', '', '', '', '', '', 28),
+('', '', '', '', '', '', '', '', '', '', 29);
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -153,7 +155,7 @@ ALTER TABLE `news`
 -- Indexen voor tabel `signup`
 --
 ALTER TABLE `signup`
-  ADD PRIMARY KEY (`userID`);
+  ADD PRIMARY KEY (`loginID`);
 
 --
 -- AUTO_INCREMENT voor geëxporteerde tabellen
@@ -181,7 +183,7 @@ ALTER TABLE `news`
 -- AUTO_INCREMENT voor een tabel `signup`
 --
 ALTER TABLE `signup`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `loginID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
